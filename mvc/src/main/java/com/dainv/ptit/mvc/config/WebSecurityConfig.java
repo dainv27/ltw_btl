02321f4ab916d6b32e4f7551af60d0685d99479e
dainv <nguyenvandaik57@gmail.com>
2025-04-09 16:31:44 +0700

    Update source code

--- mvc/src/main/java/com/dainv/ptit/mvc/config/WebSecurityConfig.java
+++ mvc/src/main/java/com/dainv/ptit/mvc/config/WebSecurityConfig.java
@@ -1,7 +1,43 @@
-//package com.dainv.ptit.mvc.config;
-//
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//
-//@EnableWebSecurity
-//public class WebSecurityConfig {
-//}
+package com.dainv.ptit.mvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(requests -> requests
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
